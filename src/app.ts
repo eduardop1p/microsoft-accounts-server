0ab9cdbdf91e6b42b124d30f8820de8c780a838f
eduardop1p <el702985@gmail.com>
2025-12-28 23:34:16 -0300

    melhorias

--- src/app.ts
+++ src/app.ts
@@ -37,7 +37,7 @@ class App {
   }
 
   private async middlewares() {
-    this.app.use(cors({ origin: '*' }));
+    this.app.use(cors(this.corsOptions()));
     this.app.use(helmet(this.helmetPolicy()));
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: true }));
@@ -171,7 +171,8 @@ class App {
   private corsOptions() {
     const options: CorsOptions = {
       origin: (origin, cb) => {
-        if (!origin || this.allowOrigins.includes(origin)) {
+        // if (!origin || this.allowOrigins.includes(origin)) {
+        if (!origin || !this.allowOrigins.includes(origin)) {
           //  Request from localhost will pass
           cb(null, true);
           return;
